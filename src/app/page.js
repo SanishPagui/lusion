9877e96dfe35b50da0cfe3428f5297cfe1a70b07
Sanish <sanishpagui162@gmail.com>
2024-09-21 16:20:21 +0530

    lenisss

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,30 @@
-'use client'
+"use client"
 import React from 'react'
 import Hero from './Components/Hero'
 import Navbar from './Components/Navbar'
-
+import Lenis from 'lenis'
 
 const page = () => {
+  useEffect(()=>{
+    const lenis=new Lenis({
+      
+      duration:2,
+      direction:'vertical',
+      smooth: true,
+      easing:(t)=>Math.min(1,1.001-Math.pow(2,-10*t)),
+      gestureDirection:'vertical',
+      smoothTouch:false,
+      scrub:10,
+    })
+    function raf(time){
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+    return()=>{
+      lenis.destroy()
+    }
+  })
   return (
     <div className='w-full h-full bg-[#F0F1FA]'>
       <Navbar/>
