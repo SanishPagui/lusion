399746d080f8d4f7e3a7374e1f6ddb8798e0f0ca
Sanish <sanishpagui162@gmail.com>
2024-09-21 16:29:38 +0530

    lenisss

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,18 @@
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
 import Hero from './Components/Hero'
 import Navbar from './Components/Navbar'
 import Lenis from 'lenis'
 
 const page = () => {
   const lenis = new Lenis()
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-raf()
+  useEffect(()=>{
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    raf()
+  })
 
   return (
     <div className='w-full h-full bg-[#F0F1FA]'>
